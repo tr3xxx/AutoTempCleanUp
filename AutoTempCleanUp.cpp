0db9277df9c4dc267741971a6be9e11f0ddaac20
tr3xxx <leon@tr3x.xyz>
2023-03-05 18:17:23 +0100

    main push

--- AutoTempCleanUp.cpp
+++ AutoTempCleanUp.cpp
@@ -1,33 +1,43 @@
 #include <Windows.h>
 #include <shellapi.h>
+#include <Shellapi.h>
+#include <filesystem>
 #include <string>
 #include <lmcons.h>
 #include <iostream>
+#include <fstream>
+#include <tchar.h>
 
 using namespace std;
 
 #define ID_TRAYICON 1
-#define ID_AUTOSTART 2
 #define ID_CLEANUP 3
 #define ID_INFO 4
 #define ID_EXIT 5
+#define IDI_ICON 101
 
-// Function to display a message box with information about the program
-void ShowInfo(HWND hwnd) {
-    LPCWSTR url = TEXT("https://github.com/tr3xxx");
+/**
+ * @brief Opens a browser window which opens the link to the GitHub repo
+ */
+void ShowInfo() {
+    LPCWSTR url = TEXT("https://github.com/tr3xxx/AutoTempCleanUp");
     ShellExecute(NULL, L"open", url, NULL, NULL, SW_SHOWNORMAL);
 }
 
 
-// Function to perform the cleanup operation
+
+/**
+* @brief Cleans up the users temp and %temp% folder by using windows clean up tool
+**/
 void CleanUp() {
-    
     string temp_path = "C:\\Windows\\Temp\\*";
     
     char username[UNLEN + 1];
     DWORD username_len = UNLEN + 1;
     string user_temp_path = "C:\\Users\\<username>\\AppData\\Local\\Temp\\*"; 
     
+
+    // TODO: Change the loop the a replace function
     if (GetUserNameA(username, &username_len))
     {
 		for (int i = 0; i < user_temp_path.length(); i++)
@@ -39,29 +49,74 @@ void CleanUp() {
 		}
     }
     
+
+    // Run the cmd command
     system(("cmd /c \"cleanmgr /sagerun:1 & del " + temp_path + " /q & del " + user_temp_path + " /q\"").c_str());
+    
 }
 
-// Function to add or remove the program from autostart
-void SetAutoStart(bool enable) {
-    HKEY hkey;
-    wstring keypath = L"SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run";
-    wstring keyname = L"TempCleaner";
-    wstring keyvalue = L"C:\\TempCleaner\\TempCleaner.exe"; // replace this with the actual path to the executable file
+/**
+* @brief Displays a context menu when the user left-clicks or right-clicks on the trayicon.
+* @param hwnd the handle to the window that will be associated with the context menu
+**/
+void openPopUp(HWND hwnd) {
+    // Create a new popup menu
+    HMENU hMenu = CreatePopupMenu();
 
-    if (RegOpenKeyEx(HKEY_CURRENT_USER, keypath.c_str(), 0, KEY_ALL_ACCESS, &hkey) == ERROR_SUCCESS) {
-        if (enable) {
-            RegSetValueEx(hkey, keyname.c_str(), 0, REG_SZ, (LPBYTE)keyvalue.c_str(), (DWORD)(keyvalue.size() + 1));
-        }
-        else {
-            RegDeleteValue(hkey, keyname.c_str());
-        }
-        RegCloseKey(hkey);
-    }
+    // Add the "CleanUp Now" menu item
+    wstring cleanup = L"CleanUp Now";
+    AppendMenu(hMenu, MF_STRING, ID_CLEANUP, cleanup.c_str());
+
+    // Add the "Info" menu item
+    wstring info = L"Info";
+    AppendMenu(hMenu, MF_STRING, ID_INFO, info.c_str());
+
+    // Add a separator between the "Info" and "Exit" menu items
+    wstring separator = L"";
+    AppendMenu(hMenu, MF_SEPARATOR, 0, separator.c_str());
+
+    // Add the "Exit" menu item
+    wstring exit = L"Exit";
+    AppendMenu(hMenu, MF_STRING, ID_EXIT, exit.c_str());
+
+    // Bring the window to the foreground to ensure the menu is displayed in front of other windows
+    SetForegroundWindow(hwnd);
+
+    // Get the current cursor position
+    POINT pt;
+    GetCursorPos(&pt);
+
+    // Create a NOTIFYICONIDENTIFIER structure for the tray icon
+    NOTIFYICONIDENTIFIER nid;
+    nid.cbSize = sizeof(NOTIFYICONIDENTIFIER);
+    nid.hWnd = hwnd;
+    nid.uID = ID_TRAYICON;
+
+    // Get the bounding rectangle of the taskbar button associated with the tray icon
+    RECT taskbarRect;
+    Shell_NotifyIconGetRect(&nid, &taskbarRect);
+
+    // Calculate the position of the menu above the tray icon
+    POINT menuPos = pt;
+
+    // Show the popup menu at the calculated position
+    TrackPopupMenu(hMenu, TPM_RIGHTALIGN | TPM_BOTTOMALIGN, menuPos.x, menuPos.y, 0, hwnd, NULL);
+
+    // Destroy the menu to free up resources
+    DestroyMenu(hMenu);
 }
 
-// Window procedure for the main window
+/**
+*
+* @brief Window procedure for the main window. Handles creation and destruction of the system tray icon, and events related to the icon.
+* @param hwnd The handle to the window.
+* @param uMsg The message identifier.
+* @param wParam Additional message information.
+* @param lParam Additional message information.
+* @return The result of the message processing and depends on the message sent.
+*/
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+    // Define a static NOTIFYICONDATA variable to hold information about the tray icon
     static NOTIFYICONDATA nid;
     switch (uMsg) {
     case WM_CREATE: {
@@ -71,9 +126,8 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) 
         nid.uID = ID_TRAYICON;
         nid.uFlags = NIF_ICON | NIF_MESSAGE | NIF_TIP;
         nid.uCallbackMessage = WM_USER + 1;
-        
-       // nid.hIcon = LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_APPLICATION));
-        HICON hIcon = static_cast<HICON>(LoadImage(NULL, L"icon.ico", IMAGE_ICON, 0, 0, LR_LOADFROMFILE));
+        // Load the icon from the resources and set it as the tray icon
+        HICON hIcon = static_cast<HICON>(LoadImage(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_ICON), IMAGE_ICON, 0, 0, LR_DEFAULTSIZE));
         if (hIcon != NULL) {
             nid.hIcon = hIcon;
         }
@@ -81,7 +135,8 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) 
             nid.hIcon = LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_APPLICATION));
         }
 
-		wstring tip = L"TempCleaner";
+        // Set the tooltip text for the tray icon and add it to the system tray
+		wstring tip = L"AutoTempCleanUp";
         lstrcpy(nid.szTip, tip.c_str());
         Shell_NotifyIcon(NIM_ADD, &nid);
         break;
@@ -89,45 +144,13 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) 
     case WM_USER + 1: {
         // Handle system tray icon events
         switch (LOWORD(lParam)) {
-            case WM_LBUTTONDOWN | WM_RBUTTONDOWN: {
-                // Display a context menu when the user left-clicks or right-clicks on the icon
-                HMENU hMenu = CreatePopupMenu();
-                wstring autostart = L"Autostart";
-                AppendMenu(hMenu, MF_STRING, ID_AUTOSTART, autostart.c_str());
-                wstring cleanup = L"CleanUp Now";
-                AppendMenu(hMenu, MF_STRING, ID_CLEANUP, cleanup.c_str());
-                wstring info = L"Info";
-                AppendMenu(hMenu, MF_STRING, ID_INFO, info.c_str());
-                wstring separator = L"";
-                AppendMenu(hMenu, MF_SEPARATOR, 0, separator.c_str());
-                wstring exit = L"Exit";
-                AppendMenu(hMenu, MF_STRING, ID_EXIT, exit.c_str());
-                SetForegroundWindow(hwnd);
-                POINT pt;
-                GetCursorPos(&pt);
-
-                // Create a NOTIFYICONIDENTIFIER structure for the tray icon
-                NOTIFYICONIDENTIFIER nid;
-                nid.cbSize = sizeof(NOTIFYICONIDENTIFIER);
-                nid.hWnd = hwnd;
-                nid.uID = ID_TRAYICON;
-
-                // Get the bounding rectangle of the taskbar button associated with the tray icon
-                RECT taskbarRect;
-                Shell_NotifyIconGetRect(&nid, &taskbarRect);
-
-                // Calculate the position of the menu above the tray icon
-                POINT menuPos;
-                menuPos.x = taskbarRect.left + (taskbarRect.right - taskbarRect.left) / 2;
-                menuPos.y = taskbarRect.top + (taskbarRect.bottom - taskbarRect.top) / 2;
-               
-				printf("menuPosX", menuPos.x);
-				printf("menuPosY", menuPos.y);
-
-                // Show the popup menu
-                TrackPopupMenu(hMenu, TPM_RIGHTALIGN | TPM_BOTTOMALIGN, menuPos.x, menuPos.y, 0, hwnd, NULL);
-
-                DestroyMenu(hMenu);
+            // Open the popup menu when the user left - clicks or right - clicks on the tray icon
+            case WM_LBUTTONDOWN: {
+				openPopUp(hwnd);
+                break;
+            }
+            case WM_RBUTTONDOWN: {
+                openPopUp(hwnd);
                 break;
             }
         }
@@ -136,24 +159,36 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) 
     case WM_COMMAND: {
         // Handle menu commands
         switch (LOWORD(wParam)) {
-            case ID_AUTOSTART: {
-                // Add or remove the program from autostart
-                bool enabled = !IsDlgButtonChecked(hwnd, ID_AUTOSTART);
-                SetAutoStart(enabled);
-                CheckDlgButton(hwnd, ID_AUTOSTART, enabled ? BST_CHECKED : BST_UNCHECKED);
-                break;
-            }
             case ID_CLEANUP: {
-                // Perform the cleanup operation
+                // Perform the cleanup operation and show a notification
                 CleanUp();
-			    wstring msg = L"TempCleaner has cleaned up your temp files.";
-			    wstring title = L"TempCleaner";
-                MessageBox(hwnd, msg.c_str(),title.c_str(), MB_OK | MB_ICONINFORMATION);
+                
+                NOTIFYICONDATA nid = {};
+
+                HICON hIcon = LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_ICON));
+                if (hIcon != NULL) {
+                    nid.hIcon = hIcon;
+                }
+                else {
+                    nid.hIcon = LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_APPLICATION));
+                }
+
+                wstring msg = L"AutoTempCleaner cleaned up your temporary files successfully";
+                wstring title = L"AutoTempCleanUp";
+                lstrcpy(nid.szInfo, msg.c_str());
+                lstrcpy(nid.szInfoTitle, title.c_str());
+
+                nid.cbSize = sizeof(nid);
+                nid.hWnd = hwnd;
+                nid.uFlags = NIF_INFO;
+                nid.dwInfoFlags = NIIF_INFO;
+                Shell_NotifyIcon(NIM_ADD, &nid);
+
                 break;
             }
             case ID_INFO: {
-                // Show information about the program
-                ShowInfo(hwnd);
+                // Opens the GitHub repo
+                ShowInfo();
                 break;
             }
             case ID_EXIT: {
@@ -175,21 +210,70 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) 
     return DefWindowProc(hwnd, uMsg, wParam, lParam);
 }
 
-// Entry point of the program
+
+/**
+* @brief puts the executable in the autostart after checking if its alreay in
+**/
+void Autostart() {
+    // Get the path to the executable
+    TCHAR szPath[MAX_PATH];
+    GetModuleFileName(NULL, szPath, MAX_PATH);
+
+    // Open the Run key
+    HKEY hKey;
+    RegOpenKeyEx(HKEY_CURRENT_USER, TEXT("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run"), 0, KEY_QUERY_VALUE | KEY_SET_VALUE, &hKey);
+
+    // Check if the executable path is already in the Run key
+    TCHAR szValue[MAX_PATH];
+    DWORD dwSize = sizeof(szValue);
+    BOOL bExists = (RegQueryValueEx(hKey, TEXT("AutoTempCleanUp"), NULL, NULL, (LPBYTE)szValue, &dwSize) == ERROR_SUCCESS);
+
+    if (!bExists)
+    {
+        // Add the executable path to the Run key
+        RegSetValueEx(hKey, TEXT("AutoTempCleanUp"), 0, REG_SZ, (LPBYTE)szPath, (_tcslen(szPath) + 1) * sizeof(TCHAR));
+    }
+
+    // Close the key
+    RegCloseKey(hKey);
+}
+
+
+
+/**
+*@brief Entry point of the program. This function initializes the program by calling the Autostart and
+        CleanUp functions, creates the main window, and enters the message loop to handle window messages.
+*@param hInstance The handle to the current instance of the program.
+*@param hPrevInstance The handle to the previous instance of the program.
+*@param lpCmdLine The command-line arguments passed to the program.
+*@param nCmdShow Controls how the window is to be shown.
+*@return The exit code of the program.
+*/
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+    // Call the Autostart function to check and set up the program's autostart feature
+    Autostart();
+
+    // Call the CleanUp function to delete any temporary files 
+    CleanUp();
+
     // Create the main window
     WNDCLASS wc = { 0 };
-    wc.lpfnWndProc = WindowProc;
+    wc.lpfnWndProc = WindowProc; // Assign the function that will handle the window messages
     wc.hInstance = hInstance;
-    wc.lpszClassName = L"TempCleaner";
-    RegisterClass(&wc);
+    wc.lpszClassName = L"TempCleaner"; // Set the name of the window class
+    RegisterClass(&wc); // Register the window class with the system
+
+    // Create the main window with default settings
     HWND hwnd = CreateWindowW(L"TempCleaner", L"", WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, NULL, NULL, hInstance, NULL);
+
     // Show the main window and enter the message loop
-    ShowWindow(hwnd, SW_HIDE);
+    ShowWindow(hwnd, SW_HIDE); // Hide the window
     MSG msg;
     while (GetMessage(&msg, NULL, 0, 0)) {
         TranslateMessage(&msg);
         DispatchMessage(&msg);
     }
+
+    // Return the exit code of the program
     return (int)msg.wParam;
 }
